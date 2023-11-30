c483db4888fac21f29d592c163724768b955bca6
liupengfei <liupengfei@reyun.com>
2023-11-30 10:26:26 +0800

    feat: init

---
+++ dist/lib/default-code.js
@@ -0,0 +1,144 @@
+const code = [
+  {
+    name: "基础用法",
+    details: "表格的最简单用法。",
+    template: `<template>
+    
+    <Table :columns="columns1" :data="data1"></Table>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                columns1: [
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                data1: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+
+`
+  },
+  {
+    name: "固定表头",
+    details:
+      "通过设置属性 height 给表格指定高度后，会自动固定表头。当纵向内容过多时可以使用。",
+    template: `<template>
+    
+    <Table height="200" :columns="columns1" :data="data2"></Table>
+</template>
+<script>
+    export default {
+        data () {
+            return {
+                columns1: [
+                    {
+                        title: 'Name',
+                        key: 'name'
+                    },
+                    {
+                        title: 'Age',
+                        key: 'age'
+                    },
+                    {
+                        title: 'Address',
+                        key: 'address'
+                    }
+                ],
+                data2: [
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    },
+                    {
+                        name: 'John Brown',
+                        age: 18,
+                        address: 'New York No. 1 Lake Park',
+                        date: '2016-10-03'
+                    },
+                    {
+                        name: 'Jim Green',
+                        age: 24,
+                        address: 'London No. 1 Lake Park',
+                        date: '2016-10-01'
+                    },
+                    {
+                        name: 'Joe Black',
+                        age: 30,
+                        address: 'Sydney No. 1 Lake Park',
+                        date: '2016-10-02'
+                    },
+                    {
+                        name: 'Jon Snow',
+                        age: 26,
+                        address: 'Ottawa No. 2 Lake Park',
+                        date: '2016-10-04'
+                    }
+                ]
+            }
+        }
+    }
+</script>
+`
+  }
+];
+
+export default code;
---
+++ dist/precache-manifest.a5f959faf88727e0178bac3f79ce0926.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "9816503e76a29d32c704",
+    "url": "dist/css/app.432784f4.css"
+  },
+  {
+    "revision": "9816503e76a29d32c704",
+    "url": "dist/js/app.f344b572.js"
+  },
+  {
+    "revision": "3468e1cd20bc971737da",
+    "url": "dist/css/chunk-fde440ca.2b228543.css"
+  },
+  {
+    "revision": "3468e1cd20bc971737da",
+    "url": "dist/js/chunk-fde440ca.89e6e2b7.js"
+  },
+  {
+    "revision": "d2bfff577ffe29740181",
+    "url": "dist/css/chunk-vendors.4a6ca819.css"
+  },
+  {
+    "revision": "d2bfff577ffe29740181",
+    "url": "dist/js/chunk-vendors.c4d756cc.js"
+  },
+  {
+    "revision": "0482da2b292158e3e8bd11e0b44ec635",
+    "url": "dist/img/yyf.0482da2b.jpeg"
+  },
+  {
+    "revision": "b8c1dda5792fb93db4f91fffb1a88ad5",
+    "url": "dist/img/login-pic.b8c1dda5.png"
+  },
+  {
+    "revision": "b01123a424b94b2796d445ebae2d5e7d",
+    "url": "dist/img/common-table-design.b01123a4.png"
+  },
+  {
+    "revision": "5a57c6310eadf4f69b6c7543c7c5ac28",
+    "url": "dist/img/ad-basic-table.5a57c631.png"
+  },
+  {
+    "revision": "b7fd73402f3690a18c80eac5cc898bf3",
+    "url": "dist/img/myf.b7fd7340.jpeg"
+  },
+  {
+    "revision": "dad891fefe366453c8e943320808dde1",
+    "url": "dist/img/product.dad891fe.png"
+  },
+  {
+    "revision": "f19b1cfc490baed3630830704fb1bef1",
+    "url": "dist/img/lpf.f19b1cfc.png"
+  },
+  {
+    "revision": "bb98bb9a1b87bcc0ff0b28d3c9b2121f",
+    "url": "dist/img/info-config-modal.bb98bb9a.png"
+  },
+  {
+    "revision": "e4cd3ddf6ec85b351189141c7f333bcd",
+    "url": "dist/img/layout-module.e4cd3ddf.png"
+  },
+  {
+    "revision": "3d48c901a55488fcf8efb7063970f706",
+    "url": "dist/img/asset-basic-table.3d48c901.png"
+  },
+  {
+    "revision": "68c7dc3d96d21b1471f8d4497219bdb7",
+    "url": "dist/img/mcf.68c7dc3d.jpg"
+  },
+  {
+    "revision": "ab39270b7875102d99c5960b3adc67ba",
+    "url": "dist/img/components.ab39270b.svg"
+  },
+  {
+    "revision": "7f901627e08b9fbd6120157fb5e451c1",
+    "url": "dist/img/zb.7f901627.jpeg"
+  },
+  {
+    "revision": "cef5765366ce1a18597518253e803464",
+    "url": "dist/img/zss.cef57653.jpg"
+  },
+  {
+    "revision": "f939893655e779f1a7e297f5c8ba12cb",
+    "url": "dist/img/cwy.f9398936.png"
+  },
+  {
+    "revision": "fdcb6dd65120c5acce106d5317a34064",
+    "url": "dist/img/common-table.fdcb6dd6.png"
+  },
+  {
+    "revision": "201800d4ecc85f443dadd285b1d74fa2",
+    "url": "dist/img/style.201800d4.svg"
+  },
+  {
+    "revision": "cc059e76929212619f1464bfa54f33ab",
+    "url": "dist/img/lxy.cc059e76.jpg"
+  },
+  {
+    "revision": "2cbdde2df329b32fe05737ba15e5f55c",
+    "url": "dist/img/fw.2cbdde2d.jpeg"
+  },
+  {
+    "revision": "83b4e9859c9e01583b079b3b14b9d6a5",
+    "url": "dist/img/lyp.83b4e985.jpg"
+  },
+  {
+    "revision": "661593833bc00b15f7b80d0a6d6c0ec2",
+    "url": "dist/index.html"
+  },
+  {
+    "revision": "b8c1dda5792fb93db4f91fffb1a88ad5",
+    "url": "dist/img/login-pic.png"
+  },
+  {
+    "revision": "49d16de1449e6099d04a6aacf4bfff72",
+    "url": "dist/img/login-logo.png"
+  },
+  {
+    "revision": "05f8b46b83f7ee18f89aba66240681fb",
+    "url": "dist/lib/default-code.js"
+  },
+  {
+    "revision": "efe3bdae47f32adcda9e9c848d82f94d",
+    "url": "dist/lib/random_str.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "dist/robots.txt"
+  }
+];
